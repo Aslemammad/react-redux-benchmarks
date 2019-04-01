63112734563b2c3e410411201256031cbba56952
daishi <daishi@axlight.com>
2019-04-01 23:44:48 +0900

    change to reactive-react-redux

--- sources-patches/create-variants.sh
+++ sources-patches/create-variants.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 
-if [ ! -e sources/tree-view-rher ]; then
-	cp -r sources/tree-view sources/tree-view-rher
-	patch -d sources/tree-view-rher -p2 < sources-patches/tree-view-rher
+if [ ! -e sources/tree-view-useReduxState ]; then
+	cp -r sources/tree-view sources/tree-view-useReduxState
+	patch -d sources/tree-view-useReduxState -p2 < sources-patches/tree-view-rher
 fi
 
-#if [ ! -e sources/tree-view-rher-simple ]; then
-#	cp -r sources/tree-view sources/tree-view-rher-simple
-#	patch -d sources/tree-view-rher-simple -p2 < sources-patches/tree-view-rher-simple
-#fi
-
-#if [ ! -e sources/tree-view-dumb ]; then
-#	cp -r sources/tree-view sources/tree-view-dumb
-#	patch -d sources/tree-view-dumb -p2 < sources-patches/tree-view-dumb
-#fi
-
-#if [ ! -e sources/tree-view-memo ]; then
-#	cp -r sources/tree-view sources/tree-view-memo
-#	patch -d sources/tree-view-memo -p2 < sources-patches/tree-view-memo
-#fi
-
-#if [ ! -e sources/tree-view-rrh ]; then
-#	cp -r sources/tree-view sources/tree-view-rrh
-#	patch -d sources/tree-view-rrh -p2 < sources-patches/tree-view-rrh
-#fi
-
-if [ ! -e sources/deeptree-rher ]; then
-	cp -r sources/deeptree sources/deeptree-rher
-	patch -d sources/deeptree-rher -p2 < sources-patches/deeptree-rher
+if [ ! -e sources/tree-view-useReduxStateSimple ]; then
+	cp -r sources/tree-view sources/tree-view-useReduxStateSimple
+	patch -d sources/tree-view-useReduxStateSimple -p2 < sources-patches/tree-view-rherSimple
 fi
 
-if [ ! -e sources/twitter-lite-rher ]; then
-	cp -r sources/twitter-lite sources/twitter-lite-rher
-	patch -d sources/twitter-lite-rher -p2 < sources-patches/twitter-lite-rher
+if [ ! -e sources/tree-view-dumb ]; then
+	cp -r sources/tree-view sources/tree-view-dumb
+	patch -d sources/tree-view-dumb -p2 < sources-patches/tree-view-dumb
 fi
 
-if [ ! -e sources/stockticker-rher ]; then
-	cp -r sources/stockticker sources/stockticker-rher
-	patch -d sources/stockticker-rher -p2 < sources-patches/stockticker-rher
+if [ ! -e sources/tree-view-memo ]; then
+	cp -r sources/tree-view sources/tree-view-memo
+	patch -d sources/tree-view-memo -p2 < sources-patches/tree-view-memo
 fi
 
-if [ ! -e sources/forms-rher ]; then
-	cp -r sources/forms sources/forms-rher
-	patch -d sources/forms-rher -p2 < sources-patches/forms-rher
+if [ ! -e sources/tree-view-rrh ]; then
+	cp -r sources/tree-view sources/tree-view-rrh
+	patch -d sources/tree-view-rrh -p2 < sources-patches/tree-view-rrh
 fi
 
-if [ ! -e sources/deeptree-nested-rher ]; then
-	cp -r sources/deeptree-nested sources/deeptree-nested-rher
-	patch -d sources/deeptree-nested-rher -p2 < sources-patches/deeptree-nested-rher
+if [ ! -e sources/deeptree-useReduxState ]; then
+	cp -r sources/deeptree sources/deeptree-useReduxState
+	patch -d sources/deeptree-useReduxState -p2 < sources-patches/deeptree-rher
+fi
+
+if [ ! -e sources/twitter-lite-useReduxState ]; then
+	cp -r sources/twitter-lite sources/twitter-lite-useReduxState
+	patch -d sources/twitter-lite-useReduxState -p2 < sources-patches/twitter-lite-rher
+fi
+
+if [ ! -e sources/stockticker-useReduxState ]; then
+	cp -r sources/stockticker sources/stockticker-useReduxState
+	patch -d sources/stockticker-useReduxState -p2 < sources-patches/stockticker-rher
+fi
+
+if [ ! -e sources/forms-useReduxState ]; then
+	cp -r sources/forms sources/forms-useReduxState
+	patch -d sources/forms-useReduxState -p2 < sources-patches/forms-rher
+fi
+
+if [ ! -e sources/deeptree-nested-useReduxState ]; then
+	cp -r sources/deeptree-nested sources/deeptree-nested-useReduxState
+	patch -d sources/deeptree-nested-useReduxState -p2 < sources-patches/deeptree-nested-rher
 fi
 
 if [ ! -e sources/deeptree-nested-rrh ]; then
